f3270bc37aa7dcde05726e756d3397bcf77def88
goraisonali2017 <goraisonali2017@gmail.com>
2021-10-05 06:14:40 +0000

    changing

--- springapp/src/main/java/com/examly/springapp/Controller/UserModelController.java
+++ springapp/src/main/java/com/examly/springapp/Controller/UserModelController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserModelController {
 
-    @RequestMapping("/hello")
-    public String greet() {
-        return "Good Day";
-    }
+   
+    
 }
