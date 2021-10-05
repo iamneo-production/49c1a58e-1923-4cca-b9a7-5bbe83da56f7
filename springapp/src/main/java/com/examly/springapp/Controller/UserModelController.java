5ffe509a3e570450e9ea3a0ef35743e383c512f0
goraisonali2017 <goraisonali2017@gmail.com>
2021-10-05 05:38:39 +0000

    setup

---
+++ springapp/src/main/java/com/examly/springapp/Controller/UserModelController.java
@@ -0,0 +1,12 @@
+package com.examly.springapp.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserModelController {
+
+    @RequestMapping("/hello")
+    public String greet() {
+        return "Good Day";
+    }
+}
