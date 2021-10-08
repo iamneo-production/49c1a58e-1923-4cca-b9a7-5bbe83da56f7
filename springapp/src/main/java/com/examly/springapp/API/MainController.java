43ff1f4a7855b1686bfa3119a35bcea044e63869
Apoorv <43790071+apoorv-rawat@users.noreply.github.com>
2021-10-08 17:29:26 +0530

    Added controller for product

--- springapp/src/main/java/com/examly/springapp/API/MainController.java
+++ springapp/src/main/java/com/examly/springapp/API/MainController.java
@@ -1,15 +1,13 @@
-package com.examly.springapp.API;
+package com.examly.springapp.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class MainController {
-    
     @RequestMapping("/hello")
     public String sayHello() {
-        return "HI";
+        return "Hi";
     }
 
 }
---
+++ springapp/src/main/java/com/examly/springapp/API/ProductService.java
@@ -0,0 +1,11 @@
+package com.examly.springapp.api;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    
+    public String getAllData() {
+        return "This is all the data";
+    }
+}
