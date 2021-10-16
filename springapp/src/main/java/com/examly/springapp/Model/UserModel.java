885317a0f8b31822c6ec9121ccf51a41fc8fcf48
Arup Maji <arupmaji02017@gmail.com>
2021-10-16 23:30:12 +0530

    adding initial files

--- springapp/src/main/java/com/examly/springapp/Model/UserModel.java
+++ springapp/src/main/java/com/examly/springapp/Model/UserModel.java
@@ -13,8 +13,8 @@ public class UserModel {
     @Column(name = "first_name", nullable = false, length = 40)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false, length = 40)
-    private String lastName;
+    @Column(name = "role", nullable = false, length = 40)
+    private String role;
 
     @Column(nullable = false, unique = true, length = 50)
     private String email;
